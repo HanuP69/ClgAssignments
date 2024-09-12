2178274c9b8ee6fec9ba55e705d4c9a8b807c488
HanuP69 <honeypandey680@gmail.com>
2024-09-12 17:27:42 +0530

    Update 1.18.c

--- Ass1/1.18.c
+++ Ass1/1.18.c
@@ -3,10 +3,11 @@ int main(){
 printf("Enter year \n");
 int year;
 scanf("%d", &year);
-if(year%400==0||year%4==0){
+if(year%100!=0&&year%4==0||year%100==0&&year%400==0){
     printf("Leap year!");
 }
 else{
     printf("Not a leap year!");
 }
 }
+/*Credits to Irfan Hashmi(roll: 29) for pointing out mistake in earlier code which caused 1900 to be a leap year*/
